454176fe08b3f82e3d75b9caab2806562e618398
thpixel-dev <thiago_arnold@estudante.sesisenai.org.br>
2025-02-18 17:18:01 -0300

    add pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'emoji',
+        short_name: 'emj',
+        description: 'um game de emoji',
+        theme_color: 'image/png',
+        icons: [
+          {
+            src: 'hugeicons--game192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'hugeicons--game592.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
